f207adfb0c1d05b59c6e3c4b54a36670be212965
Christina Mosnick <cmosnick07@mwc-080222.dhcp.missouri.edu>
2014-10-04 17:01:40 -0500

    Fixed mapPage link on index.ejs

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/mapPage', function(req, res){
-	//res.render();
-	});
+	res.render('mapPage.ejs');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
