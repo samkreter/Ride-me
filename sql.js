683c34921e9898396415ea3d0a535cc384ea743a
Sam Kreter <sakfy6@mail.missouri.edu>
2014-10-04 23:56:01 -0500

    worked on modualizing datbase end

--- sql.js
+++ sql.js
@@ -1,29 +1,87 @@
 var fs = require("fs");
-var file = "test.db";
+var file = "rideme.db";
 var exists = fs.existsSync(file);
 
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
 
 
+
+exports.createTables = function() {
+    console.log("Created table person");
+    db.run("CREATE TABLE person (
+				fname varchar(25),
+				lname varchar(25),
+				street varchar(40),
+				city varchar(40),
+				zipcode integer,
+				driver boolean,
+				hash varchar(32),
+				id integer PRIMARY KEY)");
+    console.log("Created table schedule");
+    db.run("CREATE TABLE schedule(
+			datetime DATETIME,
+			eventName varchar(40),
+			person_id integer,
+			FOREIGN KEY(person_id) REFERENCES person(id))");
+}
+
+
+exports.insertUser = function() {
+
+    var stmt = db.prepare("INSERT INTO person VALUES (?,?,?,?,?,?,?,?)");
+
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+
+    stmt.finalize(readAllRows);
+}
+
+exports.query = function() {
+    console.log("readAllRows lorem");
+    db.all("SELECT rowid AS id, info FROM lorem", function(err, rows) {
+        rows.forEach(function (row) {
+            console.log(row.id + ": " + row.info);
+        });
+    });
+}
+
+
+exports.closeDb = function() {
+    console.log("closeDb");
+    db.close();
+}
+
+
+
+
+
+
+
+
+
+
 exports.test = function(){
 	db.serialize(function() {
 	  if(!exists) {
 	  	console.log("new file");
-	    db.run("CREATE TABLE Stuff (thing TEXT, name varchar(20))");
+	    db.run("CREATE TABLE person (
+				fname varchar(25),
+				lname varchar(25),
+				street varchar(40),
+				city varchar(40),
+				zipcode integer,
+				driver boolean,
+				hash varchar(32),
+				id integer PRIMARY KEY)");
 	  }
+	  db.run("CREATE TABLE schedule(
+			datetime DATETIME,
+			eventName varchar(40),
+			person_id integer,
+			FOREIGN KEY(person_id) REFERENCES person(id))")
 	  
-	        var stmt = db.prepare("INSERT INTO Stuff VALUES (?, ?)");
-	  
-	//Insert random data
-	  var rnd;
-	  var names = ["sam","paul","steve","pat"];
-	  for (var i = 0; i < names.length; i++) {
-	    rnd = Math.floor(Math.random() * 10000000);
-	    stmt.run(names[i] + rnd,names[i]);
-	  }
-	  
-	stmt.finalize();
 	  db.each("SELECT rowid AS id, thing, name FROM Stuff", function(err, row) {
 	    console.log(row.id + ": " + row.thing + row.name);
 	  });
